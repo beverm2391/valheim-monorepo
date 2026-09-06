83b2b11f70e9f4f8ca93bfd5f633cdb79f913113
Ben Everman <beverman4528@gmail.com>
2026-09-05 22:49:10 -0400

    Bump Benheim to 0.1.90

--- client-mods/benheim/src/Plugin.cs
+++ client-mods/benheim/src/Plugin.cs
@@ -26,7 +26,7 @@ public sealed class Plugin : BaseUnityPlugin
 {
     public const string PluginGuid = "com.benheim.qol";
     public const string PluginName = "Benheim";
-    public const string PluginVersion = "0.1.89";
+    public const string PluginVersion = "0.1.90";
 
     internal static ManualLogSource Log { get; private set; } = null!;
 
